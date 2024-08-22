a8e967aa97c368a291e9af6d2939f773d1f564fe
Fabijuraski <105720352+Fabianaju@users.noreply.github.com>
2024-08-22 17:31:20 -0300

    Update script.js

--- css/script.js
+++ css/script.js
@@ -1,13 +1,1 @@
-document.getElementById('start-animation').addEventListener('click', function() {
-    const flower = document.querySelector('.flower');
-    const particles = document.querySelectorAll('.particles div');
-    
-    // Animação de crescimento da flor
-    flower.style.transform = 'scale(1)';
 
-    // Animação das partículas de água
-    particles.forEach(particle => {
-        particle.style.opacity = '1';
-        particle.style.transform = 'translateY(-60px)';
-    });
-});
